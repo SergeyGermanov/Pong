b2b5abad07c3173d3aa2982497b7e5cf29cc75da
Sergey Germanov <sergyleo@yahoo.com>
2020-06-06 19:36:37 +1200

    first commit

---
+++ js/sketch.js
@@ -0,0 +1,38 @@
+let playerPaddle;
+let aiPaddle;
+
+function setup() {
+    createCanvas(624, 351)
+    playerPaddle = new Paddle(26);
+    aiPaddle = new Paddle(width - 48);
+};
+
+function draw() {
+    background(0);
+    playerPaddle.display();
+    aiPaddle.display();
+
+    if (playerPaddle.isUp) {
+        playerPaddle.up();
+    } else if (playerPaddle.isDown) {
+        playerPaddle.down();
+    }
+
+
+};
+
+function keyPressed() {
+    if (keyCode === UP_ARROW) {
+        playerPaddle.isUp = true;
+    } else if (keyCode === DOWN_ARROW) {
+        playerPaddle.isDown = true;
+    }
+}
+
+function keyReleased() {
+    if (keyCode === UP_ARROW) {
+        playerPaddle.isUp = false;
+    } else if (keyCode === DOWN_ARROW) {
+        playerPaddle.isDown = false;
+    }
+}
